b3d45fc0eb1a2e0aed1fbf37ef13e929184c1a18
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-12-02 03:44:22 +0000

    Restructred project source

--- topchat/client/serializers.py
+++
@@ -1,7 +0,0 @@
-from client.models import Room
-from rest_framework import serializers
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = ('name', 'id')
