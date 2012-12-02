37cb81c512546fd43d4b9f62bd7188c4074470d9
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-12-02 02:53:06 +0000

    Renamed application in production config

--- mochatapp/mochatapp/settings/production.py
+++ mochatapp/mochatapp/settings/production.py
@@ -64,7 +64,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/var/www/mochatapp/builds/live/production/static',
+    '/var/www/topchat/builds/live/production/static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -104,7 +104,7 @@ ROOT_URLCONF = 'mochatapp.urls'
 WSGI_APPLICATION = 'mochatapp.wsgi.application'
 
 TEMPLATE_DIRS = (
-     '/var/www/builds/live/production/templates',
+     '/var/www/topchat/builds/live/production/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
