14c32ab2d3cb5a5d02d2e9bf2ff2069800adec76
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-12-23 16:19:50 +0000

    Changed room serializer to return admins as well

--- client/serializers.py
+++ client/serializers.py
@@ -1,7 +1,17 @@
-from client.models import Room
+from client.models import Room, RoomAdministrator
 from rest_framework import serializers
 
+
+class RoomAdministratorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RoomAdministrator
+        fields = ('administrator',)
+
+
 class RoomSerializer(serializers.ModelSerializer):
+    administrators = RoomAdministratorSerializer(source='roomadministrator_set')
+
     class Meta:
         model = Room
-        fields = ('name', 'id')
+        fields = ('name', 'id', 'administrators')
