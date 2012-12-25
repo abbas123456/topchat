b967bb9acc321907b2b181efb90f8c60dca50fd9
Mohammad Abbas <mohammad.abbas86@gmail.com>
2012-12-25 01:25:04 +0000

    Added administrated rooms to user rest serializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,12 +1,23 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
+from client.models import RoomAdministrator
+
+
+class RoomAdministratorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RoomAdministrator
+        fields = ('room',)
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    administrated_rooms = RoomAdministratorSerializer(source='roomadministrator_set')
+
     class Meta:
         model = User
-        fields = ('username', 'password', 'id')
+        fields = ('username', 'password', 'id', 'administrated_rooms')
 
     def save(self, save_m2m=True):
         self.object.password = make_password(self.object.password)
